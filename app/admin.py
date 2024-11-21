f83727bc1bfab9b625482d9d377e47ce0557d12a
David Burklin <david.burklin@halliburton.com>
2024-11-20 21:10:28 -0600

    Section 6 complete

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from app.models import JobPost
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'title', 'date', 'salary')
+    list_filter = ('date', 'salary', 'expiry')
+    search_fields = ['title', 'description', 'salary'] # can also use tuple
+    search_help_text = "Enter your parameter(s)"
+    #fields = (('title', 'description'), 'expiry', 'salary') #specific fields to display OR...
+    #exclude = ('title',)  # all fields excluding these.
+    #fieldsets
+    fieldsets = (
+        ('Basic Information', {
+            'fields' :('title', 'description')
+        }),
+        ('More information', {
+            'classes':('collapse', 'wide'),
+            'fields':(('expiry', 'salary'), 'slug') #like fields in a tuple will group together)
+        })
+    )
 
 # Register your models here.
+admin.site.register(JobPost, JobAdmin)
